89da80e656a6280cdf0530818049f69870d2399c
Gustav Ljungberg <g.ljungberg@live.se>
2018-11-28 22:43:51 +0100

    implemented start method

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,7 +3,7 @@ public class GuessingGame {
 
 	public static void main(String[] args) {
 		Guesser guesser = new Guesser(1000, 0);
-		System.out.println(guesser.toString());
+		guesser.start();
 	}
 
 }
