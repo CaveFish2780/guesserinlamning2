a6b5c4cff95b5cf407a1c503a5de4763a820f11e
Gustav Ljungberg <g.ljungberg@live.se>
2018-11-28 22:57:58 +0100

    modified main method

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -3,7 +3,10 @@ public class GuessingGame {
 
 	public static void main(String[] args) {
 		Guesser guesser = new Guesser(1000, 0);
-		guesser.start();
+//		guesser.start();
+		Guesser guesser2 = new Guesser(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+		guesser2.start();
+		
 	}
 
 }
